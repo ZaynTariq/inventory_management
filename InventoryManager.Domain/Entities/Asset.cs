0c3beffdbfb75daf32393f877875266c5f6f7337
ZaynTariq <145370691+ZaynTariq@users.noreply.github.com>
2025-12-27 12:28:13 +0500

    entity added

---
+++ InventoryManager.Domain/Entities/Asset.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryManager.Domain.Entities;
+
+using InventoryManager.Domain.Entities.Base;
+public class Asset : BaseEntity {
+    public string Name { get; set; } = default!;
+    public string SerialNumber { get; set; } = default!;
+    public DateTime PurchaseDate { get; set; }
+    public int CategoryId { get; set; }
+    public int LocationId { get; set; }
+    public string Status { get; set; } = default!;
+}
---
+++ InventoryManager.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace InventoryManager.Domain.Entities.Base;
+
+using System;
+public interface IEntity;
+
+public interface ITrackedEntity : IEntity {
+    public DateTime CreatedOn { get; }
+    public DateTime? ModifiedOn { get; }
+}
+
+public interface IAuditableEntity<TId> {
+    public TId? CreatedBy { get; }
+    public TId? ModifiedBy { get; }
+} 
+public class BaseEntity<TId> : ITrackedEntity, IAuditableEntity<TId> {
+    public TId Id { get; set; } = default!;
+    public TId? CreatedBy { get; set; }
+    public TId? ModifiedBy { get; set; }
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    public DateTime? ModifiedOn { get; set; }
+}
+
+
+public class BaseEntity : BaseEntity<long>;
+
